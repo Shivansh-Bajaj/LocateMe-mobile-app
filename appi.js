dd98e7bf45c8b388b5d50396ec991206d2878128
Arush Goyal <arushgyl@gmail.com>
2015-06-20 19:54:20 +0530

    improved gui

--- appi.js
+++ appi.js
@@ -17,18 +17,19 @@ function geoFindMe() {
 
       var mapp = 'https://www.google.co.in/maps/place/'+latitude+','+longitude;
 
-      var text= 'Share this Link: <br><br><a href='+mapp+'>'+ mapp +'</a>';
+      var text= '<br>Share this Link: <br><br><a href='+mapp+'>'+ mapp +'</a>';
       output.innerHTML=text;
       //lnk=document.getElementsByTagName('a');
       //lnk[3].setAttribute('target','_blank');
-      var button='<font size="6px"><b>SHARE</b></font>'
+      document.getElementById('collection').setAttribute('class','collection');
+      var button='<a class="waves-effect waves-light btn-large"><font size="6px">Share</font></a>';
       var share=document.getElementById('share');
-      share.setAttribute('class','header');
+      share.setAttribute('class','collection-item');
       share.innerHTML=button;
       document.getElementById('share').addEventListener('click',shareLocation);
       var save = document.getElementById('save');
-      save.setAttribute('class','header');
-      save.innerHTML='<font size="6px"><b>SAVE</b></font>';
+      save.setAttribute("class","collection-item");
+      save.innerHTML='<a class="waves-effect waves-light btn-large";"><font size="6px">Save</font></a>';
       document.getElementById('save').addEventListener('click',saveLocation);
       function saveLocation()
       {
@@ -91,7 +92,7 @@ function geoFindMe() {
   } 
 
   
-  output.innerHTML = "<p>Locating...</p>";
+  output.innerHTML = "<div class='progress'><div class='indeterminate'></div></div>";
   navigator.geolocation.getCurrentPosition(success, showError,{
       enableHighAccuracy : true
     });
@@ -103,8 +104,8 @@ function share1(name,map)
 }
 function places()
 {
-  document.getElementById('loc').setAttribute('class','saved_locations selected');
-  document.getElementById('home').setAttribute('class','home');
+  document.getElementById('loc').setAttribute('class','active');
+  document.getElementById('home').setAttribute('class','');
   main = document.getElementById('main');
   if(!!document.getElementById('body1'))
   {
@@ -120,13 +121,14 @@ function places()
     a = JSON.parse(localStorage.file);
     space = document.createElement('div');
     space.setAttribute('id','space');
+    space.setAttribute('class','collection')
     main.appendChild(space);
     for(i=0; i<a.length ; i++)
     {
       row = document.createElement('div');
-      row.setAttribute('class','row');
+      row.setAttribute('class','row collection-item');
       space.appendChild(row);
-      row.innerHTML='<div class="name col">' + a[i].name + '</div><div class="map col"><a href='+a[i].map+' target="_blank" >Map Link</a></div><div class="share col"><a href="#" onclick="share1(\''+a[i].name+'\',\''+a[i].map+'\');"><span class="glyphicon glyphicon-envelope"></span></a></div><div class="remove col" onclick="del('+i+');"><a href="#"><span class="glyphicon glyphicon-trash"></span></a></div>';
+      row.innerHTML='<div class="name col s4">' + a[i].name + '</div><div class="map col s4"><a href='+a[i].map+' target="_blank" >Map</a></div><div class="share col s2"><a href="#" onclick="share1(\''+a[i].name+'\',\''+a[i].map+'\');"><span class="medium mdi-content-mail"></span></a></div><div class="remove col s2" onclick="del('+i+');"><a href="#"><span class="medium mdi-action-delete"></span></a></div>';
       
     }
   }
